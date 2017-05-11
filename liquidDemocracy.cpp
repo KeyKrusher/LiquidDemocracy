6f8e469fadbdc16dea868e197e3baa0703b4f97b
period2 <period2@admins-MacBook-Air-47.local>
2017-05-11 09:27:01 -0500

    Initial Commit

---
+++ liquidDemocracy.cpp
@@ -0,0 +1,3 @@
+// Code written by Jack Clark
+// This code is the solution to the Liquid Democracy Problem
+#include <iostream>
---
+++ liquidDemocracy.py
@@ -0,0 +1,6 @@
+# Code written by Jack Clark
+# Code demontrates liquid democracy solution
+
+amount_of_people = raw_input("How many people?")
+
+array = []
