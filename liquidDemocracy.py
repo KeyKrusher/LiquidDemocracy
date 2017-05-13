79307d134b6ce1dbf08838e41a00d0d6670a5b0e
Jack "Chubs" Clark <clark.jack554@gmail.com>
2017-05-13 13:25:12 -0500

    Update liquidDemocracy.py
    Updates it and adds testing support.

--- liquidDemocracy.py
+++ liquidDemocracy.py
@@ -1,6 +1,11 @@
 # Code written by Jack Clark
 # Code demontrates liquid democracy solution
+import test
 
 amount_of_people = raw_input("How many people?")
 
 array = []
+
+# Run the test script
+if __name__ == "__name__":
+   run_test()
